06eb7d628937778be04162b4b318b7e1c225f408
Tomasz Szajna <tszajna0@gmail.com>
2016-11-08 12:45:44 +0100

    Introduced adminAreaCode
    - geocodePositionStateCode returns addresses with adminAreaCode (e.g. US state code)

--- js/geocoder.js
+++ js/geocoder.js
@@ -1,5 +1,6 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 import GoogleApi from './googleApi.js';
+import { stateToCode } from './states.js';
 
 const { RNGeocoder } = NativeModules;
 
@@ -19,6 +20,35 @@ export default {
       if (!this.apiKey || err.code !== 'NOT_AVAILABLE') { throw err; }
       return GoogleApi.geocodePosition(this.apiKey, position);
     });
+  },
+
+  /**
+   * Geocodes position and sets adminAreaCode (e.g. US state code) on best effort
+   *
+   * Although Android docs says the admin area is a code (e.g. "CA")
+   * https://developer.android.com/reference/android/location/Address.html#getAdminArea()
+   * some Android devices return localized name of the state.
+   * Therefore en_US locale is enforced.
+   *
+   * iOS returns a code in adminArea.
+   */
+  geocodePositionStateCode(position) {
+    if (!position || !position.lat || !position.lng) {
+      return Promise.reject(new Error("invalid position: {lat, lng} required"));
+    }
+
+    let q = (Platform.OS === 'android')
+      ? RNGeocoder.geocodeLocalizedPosition('en', 'US', position)
+      : RNGeocoder.geocodePosition(position);
+    return q.catch(err => {
+      if (!this.apiKey || err.code !== 'NOT_AVAILABLE') { throw err; }
+      return GoogleApi.geocodePosition(this.apiKey, position);
+    }).then(res => res.map(i => {
+      if (i.countryCode && i.adminArea && !i.adminAreaCode) {
+        i.adminAreaCode = stateToCode(i.countryCode, i.adminArea);
+      }
+      return i;
+    }));
   },
 
   geocodeAddress(address) {
